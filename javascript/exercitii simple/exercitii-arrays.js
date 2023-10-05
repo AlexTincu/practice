dcc129a9fe29c8d9cf0870aa7b2801f991cfec7f
Alex Tincu <tz.alexx@gmail.com>
2023-10-05 21:28:06 +0300

    Adaugare exercitii

---
+++ javascript/exercitii simple/exercitii-arrays.js
@@ -0,0 +1,36 @@
+/*
+    Creeaza un fisier javascript cu numele tau si acolo scrie aceste exercitii.
+
+1. Suma elementelor
+Scrie o funcție care primește un array de numere și returnează suma tuturor elementelor din array.
+
+2. Elementul maxim
+Scrie o funcție care primește un array de numere și returnează valoarea maximă din array.
+
+3. Elemente unice
+Scrie o funcție care primește un array și returnează un nou array care conține doar elementele 
+unice din array - ul inițial.
+
+4. Concatenarea array - urilor
+Scrie o funcție care primește două array - uri și returnează un nou array care conține toate 
+elementele din cele două array - uri concatenate.
+
+5. Filtrarea array - ului
+Scrie o funcție care primește un array și returnează un nou array  care conține doar elementele de tip numeric.
+
+6. Inversarea array - ului
+Scrie o funcție care primește un array și returnează un nou array care conține 
+elementele în ordine inversă.
+
+7. Căutarea elementului
+Scrie o funcție care primește un array și un element și returnează `true` dacă elementul 
+se găsește în array și `false` în caz contrar, folosind metoda includes()
+
+8. Transformarea elementelor cu map()
+Scrie o funcție care primește un array de numere și returnează un nou array în care fiecare element este dublat, folosind metoda map().
+
+9. Filtrarea elementelor cu filter()
+Scrie o funcție care primește un array de numere și returnează un nou array care conține doar 
+numerele pare, folosind metoda filter().    
+
+*/
