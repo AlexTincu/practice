cfd53838a6c5b71fabc01447de549bfe0f978ae6
Alex Tincu <tz.alexx@gmail.com>
2023-11-16 17:46:33 +0200

    modificari la solutia lui Andrei

--- javascript/DOM/05-Guess-My-Number/Lucaciuc Andrei/script.js
+++ javascript/DOM/05-Guess-My-Number/Lucaciuc Andrei/script.js
@@ -1,26 +1,33 @@
 class NumberGame {
     constructor() {
+        this.limit = 100;
         this.secretNumber = this.generateRandomNumber();
         this.attempts = 0;
         this.score = 100;
     }
 
     generateRandomNumber() {
-        return Math.floor(Math.random() * 100) + 1;
+        return Math.floor(Math.random() * this.limit) + 1;
     }
 
     checkGuess(userGuess) {
+        if (isNaN(userGuess) || userGuess < 1 || userGuess > this.limit) {
+            this.displayResult(`Please enter a valid number between 1 and ${this.limit}.`, 'danger');
+            return;
+        }
+
         this.attempts++;
-        const difference = Math.abs(userGuess - this.secretNumber);
-        this.score--;
 
         if (userGuess === this.secretNumber) {
             this.displayResult(`Congratulations! You guessed the number ${this.secretNumber} in ${this.attempts} attempts.`, 'success');
             this.disableInputAndButton();
             document.body.style.backgroundColor = '#7FFF7F'; // Green background
         } else {
+            this.score--;
+            const difference = Math.abs(userGuess - this.secretNumber);
+
             const hint = userGuess < this.secretNumber ? 'higher' : 'lower';
-            this.displayResult(`Wrong guess! Try a ${hint} number. Attempts: ${this.attempts}`, 'danger');
+            this.displayResult(`Wrong guess! <br/> Try a ${hint} number. Attempts: ${this.attempts}`, 'danger');
             this.updateScore();
             this.updateBackgroundColor(difference);
         }
@@ -32,14 +39,28 @@ class NumberGame {
     }
 
     updateScore() {
-        const scoreDiv = document.getElementById('score');
-        scoreDiv.textContent = `Score: ${this.score}`;
+        // poate fi scris intr-o singura linie mai simplu
+        document.getElementById('score').textContent = `Score: ${this.score}`;
     }
 
+    /**
+     * Idea este interesanta, dar, eu cel putin nu am o solutie la ea, pentru ca:
+     * 
+     * Nu se poate face o tranzitie de la rosu la verde astfel incat sa aiba si sens, pentru ca 
+     * intre rosu si verde nu e o diferenta liniara in functia rgb
+     * tot ce se poate face este o tranzitie de la verde inchis la verde deschis. 
+     * oricum per total experienta userului nu e placuta, si idea jocului nu e sa-ti spuna cat de departe sau aproape esti
+     * 
+     * @param {*} difference 
+     */
     updateBackgroundColor(difference) {
-        const redValue = Math.min(255, 255 - difference * 5);
-        const greenValue = Math.min(255, difference * 5);
-        document.body.style.backgroundColor = `rgb(${redValue}, ${greenValue}, 240)`;
+        // diferenta intre numere este intre 1 si this.limit (care a devenit dinamic asa cum se fac lucrurile)
+        // diferenta intre culori este intre 1 si 255
+        // deci parametrul care ma intereseaza este difference transformat din baza this.limit in baza 255
+        const difference255 = Math.floor(difference * 255 / this.limit);
+        const greenValue = 255 - difference255;
+
+        document.body.style.backgroundColor = `rgb(0, ${greenValue}, 0)`;
     }
 
     disableInputAndButton() {
@@ -55,11 +76,6 @@ const game = new NumberGame();
 function checkGuess() {
     const userGuessInput = document.getElementById('userGuess');
     const userGuess = parseInt(userGuessInput.value);
-
-    if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
-        alert('Please enter a valid number between 1 and 100.');
-        return;
-    }
 
     game.checkGuess(userGuess);
 }
